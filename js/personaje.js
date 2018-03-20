3957708ba35502409611f4584252be30c07a001c
Juande <juande.mc@redyser.com>
2018-03-20 11:29:48 +0100

    Añadido parametro id a personaje.html

--- js/personaje.js
+++ js/personaje.js
@@ -3,5 +3,19 @@ function script(){
   while( span.firstChild ) {
     span.removeChild( span.firstChild );
   }
-  span.appendChild( document.createTextNode("Xpersonaje") );
+  var id = findGetParameter("id");
+  span.appendChild( document.createTextNode(id+"personaje") );
+}
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
 }
