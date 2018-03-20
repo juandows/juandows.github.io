e84a3352edf961f970988385f1cf1693b9b0a18b
Juande <juande.mc@redyser.com>
2018-03-20 11:31:45 +0100

    Añaddo archivo javascript común

--- js/personaje.js
+++ js/personaje.js
@@ -6,16 +6,3 @@ function script(){
   var id = findGetParameter("id");
   span.appendChild( document.createTextNode(id+"personaje") );
 }
-
-function findGetParameter(parameterName) {
-    var result = null,
-        tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
-    return result;
-}
