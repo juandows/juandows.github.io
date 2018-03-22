8828603c7e3a27b295066e967b968b458c2ca3d2
Juande <juande.mc@redyser.com>
2018-03-22 12:30:44 +0100

    Añadidas imagen de personaje y nuevos personajes

--- js/personajes.js
+++ js/personajes.js
@@ -5,7 +5,7 @@ function script(){
         console.log(champions);
         var html = '';
         for (var i = 0, len = champions.length; i < len; i++) {
-            html = '<div id="champion'+champions[i]['id']+'" class="champion"></div>';
+            html = '<a href="personaje.html?id='+champions[i]['id']+'"><div id="champion'+champions[i]['id']+'" class="champion"></div></a>';
             addHtml('champions', html);
             getChampionHtml(champions[i], function(id, htmlChampion){
                 showHtml('champion'+id, htmlChampion);
@@ -19,6 +19,8 @@ function getChampionHtml(champion, callback){
     loadJSON('data/champion'+champion["id"]+'.json', function(response) {
         var championData = JSON.parse(response);
         console.log(championData);
-        callback(champion["id"], '<p>' + champion["id"]+' '+championData["name"]+ '</p>');
+        var html = '<img src="http://ddragon.leagueoflegends.com/cdn/8.6.1/img/champion/'+championData["image"]["full"]+'">';
+        html += '<p>' + champion["id"]+' '+championData["name"]+ '</p>';
+        callback(champion["id"], html);
     });
 }
