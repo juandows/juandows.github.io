e8187a538f6e3fb39fbed51d6876ba7413760707
Juande <juande.mc@redyser.com>
2018-04-04 17:42:21 +0200

    Datos en español

--- capturer/capture.py
+++ capturer/capture.py
@@ -4,7 +4,7 @@ import json
 from pprint import pprint
 import os.path
 
-apikey = "RGAPI-dc67e7ad-4d40-48cc-a60e-10c6d8336b85";
+apikey = "RGAPI-54eaa93e-1ef3-496b-8185-903d2f4fb4a5";
 url = "https://euw1.api.riotgames.com/lol/platform/v3/champions?tags=all&api_key="+apikey
 r = requests.get(url)
 
@@ -20,7 +20,7 @@ for champion in data["champions"]:
     if (os.path.isfile('data/champion'+str(champion["id"])+'.json')):
         continue
     time.sleep(1)
-    url = "https://euw1.api.riotgames.com/lol/static-data/v3/champions/"+str(champion["id"])+"?tags=all&api_key="+apikey
+    url = "https://euw1.api.riotgames.com/lol/static-data/v3/champions/"+str(champion["id"])+"?tags=all&locale=es_ES&api_key="+apikey
     r = requests.get(url)
     pprint(r.content)
     dataChampion = r.json()
