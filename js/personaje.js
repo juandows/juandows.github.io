e9ace9863aea96935faaa3218018baf32ca51740
Juande <juande.mc@redyser.com>
2018-04-06 08:48:08 +0200

    Añadida pasiva

--- js/personaje.js
+++ js/personaje.js
@@ -11,6 +11,13 @@ function script(){
       imagen = imagen.substr(0, imagen.length-4);
       showHtml('imagen', '<img src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/'+imagen+'_0.jpg">');
       showHtml('lore', championData["lore"]);
+      var pasiva = "<div><h2>Pasiva, "+championData["passive"]["name"]+"</h2>"
+          +'<img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/passive/'
+          +championData["passive"]["image"]["full"]
+          +'">'+championData["passive"]["description"]+"</div>";
+      var habilidades = pasiva;
+      console.log(habilidades);
+      showHtml('habilidades', habilidades);
   });
 
 }
